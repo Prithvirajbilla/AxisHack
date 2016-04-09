7cb0d4ad778c0024ae5e832ef3e98bf4fde409ee
wholock <prithviraj.m.billa@gmail.com>
2016-04-09 19:40:38 +0530

    first commit

---
+++ AxisHack/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AxisHack project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AxisHack.settings")
+
+application = get_wsgi_application()
---
+++ README.md
@@ -0,0 +1,1 @@
+### Axis Hackathon
